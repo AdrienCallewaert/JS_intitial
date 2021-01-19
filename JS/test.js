c5aaf5659979abc26c9a5af7cd712c71497422b8
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 12:40:34 +0100

    le reste

--- JS/test.js
+++ JS/test.js
@@ -1,10 +1,10 @@
 const readlineSync = require("readline-sync");
-let nombre1 = new Number(readlineSync.question('choose a number with a decimal: '));
+let nombre1 = new Number(readlineSync.question('choose a integers: '));
 console.log(nombre1);
 
-let nombre2 = new Number(readlineSync.question('choose a second number with a decimal: '));
+let nombre2 = new Number(readlineSync.question('choose a second integers: '));
 console.log(nombre2);
 
-let solution = (Math.trunc(nombre1)) + nombre2;
+let solution = nombre1 % nombre2;
 
 console.log('the solution is ' + solution + ' !');
