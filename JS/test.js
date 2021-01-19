b4e347e8f18315b9783ee8b57df49044b6d08ec0
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 16:23:48 +0100

    Exercise 2.6 days

--- JS/test.js
+++ JS/test.js
@@ -1,8 +1,19 @@
 const readlineSync = require("readline-sync");
-let i = 0;
-while (i != 42) {
-    let num = new Number(readlineSync.question('What is your favorite number ?'));
-    i = num;
-    
+let num = new Number(readlineSync.question('What is your number ?'));
+if ((num == 1) || (num <= 7)) {
+    if (num == 1) {
+        console.log("Monday");
+    } else if (num == 2) {
+        console.log("Tuesday");
+    } else if (num == 3) {
+        console.log("Wednesday");
+    } else if (num == 4) {
+        console.log("Thursday");
+    } else if (num == 5) {
+        console.log("Friday");
+    } else if (num == 6) {
+        console.log("Saturday");
+    } else {
+        console.log("Sunday");
+    }
 }
-
