75f921d1ea342c659264357c6c43c04535121d82
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 12:01:52 +0100

    formulaire name firstname city
    const readlineSync = require("readline-sync");
    var name = new String(readlineSync.question('your name: '));
    var firstname = new String(readlineSync.question('your firstname: '));
    var city = new String(readlineSync.question('your city: '));
    
    console.log('Your name is ' + name + ' ' + firstname + ' and you live in ' + city + ' !');

--- JS/test.js
+++ JS/test.js
@@ -1,2 +1,7 @@
-var x = 1
-console.log(x);
+const readlineSync = require("readline-sync");
+var name = new String(readlineSync.question('your name: '));
+var firstname = new String(readlineSync.question('your firstname: '));
+var city = new String(readlineSync.question('your city: '));
+
+console.log('Your name is ' + name + ' ' + firstname + ' and you live in ' + city + ' !');
+
