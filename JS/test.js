7ecf8c9eca4f4f8f1f47c4a788afae36bbef4628
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 13:30:15 +0100

    birth years & shose size
    const readlineSync = require("readline-sync");
    let nombre1 = new Number(readlineSync.question('your shoe size : '));
    console.log(nombre1);
    
    let nombre2 = new Number(readlineSync.question('your birth year: '));
    console.log(nombre2);
    
    let solution1 = nombre1 * 2 + 5;
    let solution2 = solution1 * 50 - nombre2;
    let solution3 = solution2 + 1766;
    
    console.log('the solution is ' + solution3 + ' !');

--- JS/test.js
+++ JS/test.js
@@ -1,10 +1,12 @@
 const readlineSync = require("readline-sync");
-let nombre1 = new Number(readlineSync.question('choose a integers: '));
+let nombre1 = new Number(readlineSync.question('your shoe size : '));
 console.log(nombre1);
 
-let nombre2 = new Number(readlineSync.question('choose a second integers: '));
+let nombre2 = new Number(readlineSync.question('your birth year: '));
 console.log(nombre2);
 
-let solution = nombre1 % nombre2;
+let solution1 = nombre1 * 2 + 5;
+let solution2 = solution1 * 50 - nombre2;
+let solution3 = solution2 + 1766;
 
-console.log('the solution is ' + solution + ' !');
+console.log('the solution is ' + solution3 + ' !');
