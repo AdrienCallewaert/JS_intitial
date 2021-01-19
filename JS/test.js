33cd78c0badb7eca0695e80184108a29df8b266c
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 14:35:32 +0100

    2.2  question deux conditions

--- JS/test.js
+++ JS/test.js
@@ -1,7 +1,11 @@
 const readlineSync = require("readline-sync");
-var age = new Number(readlineSync.question('Your age : '));
-if(age>17){
-    console.log('adult');
+var numbers1 = new Number(readlineSync.question('a numbers min : '));
+var numbers2 = new Number(readlineSync.question('a numbers max : '));
+var numbers3 = new Number(readlineSync.question('a current numbers : '));
+if(numbers3>numbers2){
+    console.log('wrong you are stupid');
+}else if(numbers3<numbers1){
+    console.log('wrong you are stupid');    
 }else{
-    console.log('kid');
+    console.log('ok bro, nice');
 }
