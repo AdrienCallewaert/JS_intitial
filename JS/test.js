c909adf0033eca4fb60f0c563777ef04a03c1269
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 11:41:49 +0100

    afficher VAR
    var x = 1
    console.log(x);

--- JS/test.js
+++ JS/test.js
@@ -1,8 +1,2 @@
-const readlineSync = require("readline-sync");
-let nombre1 = new Number(readlineSync.question('choose a number'));
-
-let nombre2 = new Number(readlineSync.question('choose a second number'));
-
-let solution = nombre1 % nombre2;
-
-console.log('the solution is ' + solution + "!");
+var x = 1
+console.log(x);
