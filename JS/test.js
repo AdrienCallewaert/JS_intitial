866d9dfe63e3072854faf7a6bf9c5401b1fa73b8
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 16:58:00 +0100

    2.7

--- JS/test.js
+++ JS/test.js
@@ -1,19 +1,8 @@
 const readlineSync = require("readline-sync");
-let num = new Number(readlineSync.question('What is your number ?'));
-if ((num == 1) || (num <= 7)) {
-    if (num == 1) {
-        console.log("Monday");
-    } else if (num == 2) {
-        console.log("Tuesday");
-    } else if (num == 3) {
-        console.log("Wednesday");
-    } else if (num == 4) {
-        console.log("Thursday");
-    } else if (num == 5) {
-        console.log("Friday");
-    } else if (num == 6) {
-        console.log("Saturday");
-    } else {
-        console.log("Sunday");
-    }
+let resu = 0;
+let num = new Number(readlineSync.question('Choice a number :'));
+for (let index = 0; index < num; index++) {
+    let numForAdd = new Number(readlineSync.question('Choice a new number :'));
+    resu = numForAdd + numForAdd;
 }
+console.log(resu);
