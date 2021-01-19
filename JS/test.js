8847066b925e616c3e8c0b44b2f69d8a7ad88bd0
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 11:29:30 +0100

    demander un modulo
    const readlineSync = require("readline-sync");
    let nombre1 = new Number(readlineSync.question('choose a number'));
    
    let nombre2 = new Number(readlineSync.question('choose a second number'));
    
    let solution = nombre1 % nombre2;
    
    console.log('the solution is ' + solution + "!");

--- JS/test.js
+++ JS/test.js
@@ -3,6 +3,6 @@ let nombre1 = new Number(readlineSync.question('choose a number'));
 
 let nombre2 = new Number(readlineSync.question('choose a second number'));
 
-let solution = nombre1 * nombre2;
+let solution = nombre1 % nombre2;
 
 console.log('the solution is ' + solution + "!");
