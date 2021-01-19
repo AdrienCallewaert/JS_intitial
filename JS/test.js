0cd9f82ba1fc0023dc719247f6be1d42be333f3d
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 15:47:46 +0100

    2.4 Count from 1 to 100.etc...
    const readlineSync = require("readline-sync");
    
    for (let i = 0; i <= 100; i++) {
        let mod = i % 2;
        if (mod == 0) {
            let resu = i / 2; resu
            console.log(resu);
        } else {
            let mult = i * 3;
            console.log(mult);
        }
    }

--- JS/test.js
+++ JS/test.js
@@ -1,5 +1,13 @@
-let i = 0;
-do {
-  i += 2;
-  console.log(i);
-} while (i < 100);
+
+const readlineSync = require("readline-sync");
+
+for (let i = 0; i <= 100; i++) {
+    let mod = i % 2;
+    if (mod == 0) {
+        let resu = i / 2; resu
+        console.log(resu);
+    } else {
+        let mult = i * 3;
+        console.log(mult);
+    }
+}
