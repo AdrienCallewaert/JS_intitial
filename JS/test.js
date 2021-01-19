06193f1466117a010a3d8d7582d66b67e983289f
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 14:19:28 +0100

    2.1 age

--- JS/test.js
+++ JS/test.js
@@ -1,6 +1,7 @@
 const readlineSync = require("readline-sync");
-var name = new String(readlineSync.question('1/3. your name: '));
-var city = new String(readlineSync.question('2/3. your city: '));
-var job = new String(readlineSync.question('3/3. your job: '));
-
-console.log('Your story is: ' + name + ' lives in ' + city + ' and you live in ' + job + ' !');
+var age = new Number(readlineSync.question('Your age : '));
+if(age>17){
+    console.log('adult');
+}else{
+    console.log('kid');
+}
