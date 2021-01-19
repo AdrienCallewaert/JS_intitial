e507f9668eac7bc4fd80aa940c6e91a03425ae16
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 14:50:05 +0100

    2.3 Display all even numbers between 1 and 100.

--- JS/test.js
+++ JS/test.js
@@ -1,11 +1,5 @@
-const readlineSync = require("readline-sync");
-var numbers1 = new Number(readlineSync.question('a numbers min : '));
-var numbers2 = new Number(readlineSync.question('a numbers max : '));
-var numbers3 = new Number(readlineSync.question('a current numbers : '));
-if(numbers3>numbers2){
-    console.log('wrong you are stupid');
-}else if(numbers3<numbers1){
-    console.log('wrong you are stupid');    
-}else{
-    console.log('ok bro, nice');
-}
+let i = 0;
+do {
+  i += 2;
+  console.log(i);
+} while (i < 100);
