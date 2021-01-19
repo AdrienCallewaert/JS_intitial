2b63098efeea488887a1688b44c9f1de4439186d
AdrienCallewaert <[callewaert.adrien22@gmail.com>
2021-01-19 11:25:41 +0100

    demandé une addition
    const readlineSync = require("readline-sync");
    let nombre1 = new Number(readlineSync.question('choose a number'));
    
    let nombre2 = new Number(readlineSync.question('choose a second number'));
    
    let solution = nombre1 + nombre2;
    
    console.log('the solution is ' + solution + "!");

--- JS/test.js
+++ JS/test.js
@@ -1,6 +1,8 @@
-console.log('A' + 'B');
-console.log(10 + 5);
-console.log(10 - 5);
-console.log(10 * 5);
-console.log(10 / 5);
-console.log(10 % 5); 
+const readlineSync = require("readline-sync");
+let nombre1 = new Number(readlineSync.question('choose a number'));
+
+let nombre2 = new Number(readlineSync.question('choose a second number'));
+
+let solution = nombre1 + nombre2;
+
+console.log('the solution is ' + solution + "!");
